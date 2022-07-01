1bbbf0a415a69650e171b643cff13c453def4e5c
George Maynard <george.maynard@noaa.gov>
2022-07-01 09:18:43 -0400

    new view to easily see satellite transmitters currently assigned to each vessel

---
+++ Database/Views/vessel_sat.sql
@@ -0,0 +1,22 @@
+/*
+* Title: view_vessel_sat.sql
+* Author: George Maynard
+* Contact: george.maynard@noaa.gov
+* Purpose: Displays the IMEI address(es) of the devices assigned to each vessel
+*/
+CREATE OR REPLACE VIEW vessel_sat AS
+SELECT
+  zz_vessel_visits.VESSEL_NAME,
+  zz_vessel_visits.EMOLT_NUM,
+  zz_vessel_visits.VISIT_DATE,
+  zz_installed_gear_and_addresses.EQUIPMENT_TYPE,
+  zz_installed_gear_and_addresses.MAKE,
+  zz_installed_gear_and_addresses.MODEL,
+  zz_installed_gear_and_addresses.HARDWARE_ADDRESS,
+  zz_installed_gear_and_addresses.SERIAL_NUMBER
+FROM
+  zz_vessel_visits
+  INNER JOIN
+  zz_installed_gear_and_addresses
+  ON zz_vessel_visits.VISIT_DATE = zz_installed_gear_and_addresses.max_date AND zz_vessel_visits.END_INVENTORY_ID = zz_installed_gear_and_addresses.END_INVENTORY_ID
+WHERE zz_installed_gear_and_addresses.ADDRESS_TYPE LIKE '%IMEI%'
