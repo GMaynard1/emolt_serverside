d69e339c3e3093a45f9ca9d206b0db3663b87da5
George Maynard <george.maynard@noaa.gov>
2022-07-15 10:57:45 -0400

    new temperature view, part 1 of building the ODN data portal view

---
+++ Database/Views/zz_odn_temp.sql
@@ -0,0 +1,19 @@
+/*
+* Title: odn_temperature_view.sql
+* Author: George Maynard
+* Contact: george.maynard@noaa.gov
+* Purpose: Creates a view of haul-averaged temperature data with vessel, time, and location information
+*/
+CREATE OR REPLACE VIEW zz_odn_temp AS
+SELECT
+  TOWS.VESSEL_ID,
+  TOWS.MEAN_TIME,
+  TOWS.MEAN_LATITUDE,
+  TOWS.MEAN_LONGITUDE,
+  TOWS_SUMMARY.TS_MEAN_VALUE AS MEAN_TEMPERATURE
+FROM
+  TOWS
+  INNER JOIN
+  TOWS_SUMMARY ON TOWS.TOW_ID = TOWS_SUMMARY.TOW_ID
+WHERE TOWS_SUMMARY.TS_PARAMETER="TEMP"
+;
