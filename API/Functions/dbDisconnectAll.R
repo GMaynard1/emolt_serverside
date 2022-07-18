8ef9c268dbfb2b1810b68104c9e2295f0f9bbbc0
George Maynard <george.maynard@noaa.gov>
2022-07-18 16:00:40 -0400

    closes all active MySQL connections

---
+++ API/Functions/dbDisconnectAll.R
@@ -0,0 +1,5 @@
+dbDisconnectAll <- function(){
+  ile <- length(dbListConnections(MySQL())  )
+  lapply( dbListConnections(MySQL()), function(x) dbDisconnect(x) )
+  cat(sprintf("%s connection(s) closed.\n", ile))
+}
