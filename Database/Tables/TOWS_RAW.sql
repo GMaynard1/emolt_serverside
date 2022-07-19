30926bfb8d365979ef2fef7afe54e63e26a3450d
George Maynard <george.maynard@noaa.gov>
2022-07-19 10:07:26 -0400

    rebuild of raw data structures following discussion with ODN

--- Database/Tables/TOWS_RAW.sql
+++ Database/Tables/TOWS_RAW.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `TOWS_RAW`(
   `TOWS_RAW_ID` integer NOT NULL AUTO_INCREMENT COMMENT 'UID for this table',
-  `SEGMENT_ID` integer NULL COMMENT 'Links to a record in the SEGMENTS table if one is available',
-  `TOW_ID` integer NOT NULL COMMENT 'Links to a record in the TOWS table',
+  `TOWS_SEGMENT_ID` integer NULL COMMENT 'Links to a record in the TOWS_SEGMENTS table',
   `TIMESTAMP` datetime NOT NULL COMMENT 'When the data point was collected',
   `LATITUDE` decimal(10,5) NOT NULL COMMENT 'Latitude at which the data point was collected',
   `LONGITUDE` decimal(10,5) NOT NULL COMMENT 'Longitude at which the data point was collected',
@@ -13,10 +12,7 @@ CREATE TABLE `TOWS_RAW`(
   
   PRIMARY KEY (`TOWS_RAW_ID`),
   
-  CONSTRAINT fk_towId2
-    FOREIGN KEY (`TOW_ID`)
-    REFERENCES TOWS(TOW_ID),
   CONSTRAINT fk_segId2
-    FOREIGN KEY (`SEGMENT_ID`)
-    REFERENCES SEGMENTS(SEGMENT_ID)
+    FOREIGN KEY (`TOW_SEGMENT_ID`)
+    REFERENCES TOW_SEGMENTS(TOW_SEGMENT_ID)
 )COMMENT='This table stores raw data collected from loggers';
--- Database/Tables/TOW_SEGMENTS.sql
+++ Database/Tables/TOW_SEGMENTS.sql
@@ -1,24 +1,19 @@
-CREATE TABLE `SEGMENTS`(
-  `SEGMENT_ID` integer NOT NULL AUTO_INCREMENT COMMENT 'UID for this table',
-  `TOW_ID` integer NOT NULL COMMENT 'Links to the TOWS table',
-  `INVENTORY_ID` integer NULL COMMENT 'Ties segment data to a particular instrument',
+CREATE TABLE `TOW_SEGMENTS`(
+  `TOW_SEGMENT_ID` integer NOT NULL AUTO_INCREMENT COMMENT 'UID for this table',
+  `TOW_SENSOR_ID` integer NOT NULL COMMENT 'Links to the TOWS table',
   `SEGMENT_START_TIME` datetime NOT NULL COMMENT 'The timestamp that delineates the beginning of the segment',
   `SEGMENT_END_TIME` datetime NOT NULL COMMENT 'The timestamp that delineates the end of the segment',
   `SEGMENT_TYPE` integer NOT NULL COMMENT 'Links to the SEGMENT_TYPES table',
   `SEGMENTATION_METHOD` integer NOT NULL COMMENT 'Links to the SEGMENTATION_METHODS table',
-  
+
   PRIMARY KEY (`SEGMENT_ID`),
-  
-  CONSTRAINT fk_towId
-    FOREIGN KEY (`TOW_ID`)
-    REFERENCES TOWS(TOW_ID),
+  CONSTRAINT fk_tow_sensor_id
+    FOREIGN KEY (`TOW_SENSOR_ID`)
+    REFERENCES TOW_SENSORS(TOW_SENSOR_ID),
   CONSTRAINT fk_segType
     FOREIGN KEY (`SEGMENT_TYPE`)
     REFERENCES SEGMENT_TYPES(SEGMENT_TYPE),
   CONSTRAINT fk_segMeth
     FOREIGN KEY (`SEGMENTATION_METHOD`)
     REFERENCES SEGMENTATION_METHODS(SEGMENTATION_METHOD),
-  CONSTRAINT fk_invId
-    FOREIGN KEY (`INVENTORY_ID`) 
-    REFERENCES EQUIPMENT_INVENTORY(INVENTORY_ID)
 )COMMENT='This table stores information about when tows can be broken into distinct segments (e.g., downcast vs. retrieval) and how those breakpoints are applied';
