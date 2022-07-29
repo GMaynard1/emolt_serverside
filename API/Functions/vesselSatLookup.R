a245c05ff7718521cbdc5d6a55494f527e3723c0
George Maynard <george.maynard@noaa.gov>
2022-07-29 16:03:15 -0400

    new function to lookup vessel_id using imei and serial number from satellite

---
+++ API/Functions/vesselSatLookup.R
@@ -0,0 +1,13 @@
+## Use the IMEI and serial to look up the vessel
+vesselSatLookup=function(imei,serial){
+  dbGetQuery(
+    conn=mydb,
+    statement=paste0(
+      "SELECT * FROM vessel_sat WHERE HARDWARE_ADDRESS = '",
+      imei,
+      "' AND SERIAL_NUMBER = '",
+      serial,
+      "'"
+    )
+  )$VESSEL_ID
+}
