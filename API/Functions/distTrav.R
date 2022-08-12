5648b0e42f82cdd531076bb89638b87802f0947a
George Maynard <george.maynard@noaa.gov>
2022-08-12 16:29:00 -0400

    function to calculate straight line Haversine distance between two geographic locations

---
+++ API/Functions/distTrav.R
@@ -0,0 +1,27 @@
+## Select most recent status report from database and calculate distance traveled
+## Collect the most recent status report
+library(geosphere)
+distTrav=function(vessel_id,transmit_time,dbconn,lon,lat){
+  ## Locate the most recent record
+  mr=dbGetQuery(
+    conn=dbconn,
+    statement=paste0(
+      "SELECT * FROM VESSEL_STATUS WHERE TIMESTAMP = (SELECT MAX(TIMESTAMP) FROM VESSEL_STATUS WHERE VESSEL_ID = ",
+      vessel_id,
+      "AND TIMESTAMP < '",
+      transmit_time,
+      "')"
+    )
+  )
+  ## Validate the record and calculate the Haversine distance (km) if possible
+  distance=ifelse(
+    nrow(mr)==0||is.null(mr$LATITUDE)||is.null(mr$LONGITUDE),
+    "NULL",
+    distHaversine(
+      c(lon,lat),
+      c(mr$LONGITUDE,mr$LATITUDE)
+    )/1000
+  )
+  ## Return the distance
+  return(distance)
+}
