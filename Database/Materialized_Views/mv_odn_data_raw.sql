aab7b6232daaa9da884ee750a807e6589c7eb2cb
George Maynard <george.maynard@noaa.gov>
2022-08-18 08:54:18 -0400

    raw data 'materialized views' for ODN

---
+++ Database/Materialized_Views/mv_odn_data_raw.sql
@@ -0,0 +1,19 @@
+CREATE TABLE odn_data_raw(
+  SELECT
+    zz_odn_depth_raw.VESSEL_ID,
+    zz_odn_depth_raw.FMCODE AS GEAR_TYPE,
+    zz_odn_depth_raw.TOW_ID,
+    zz_odn_depth_raw.TIMESTAMP,
+    zz_odn_depth_raw.LATITUDE,
+    zz_odn_depth_raw.LONGITUDE,
+    zz_odn_depth_raw.DEPTH,
+    zz_odn_temp_raw.TEMP
+  FROM 
+    zz_odn_depth_raw
+  INNER JOIN 
+    zz_odn_temp_raw
+  ON 
+    zz_odn_depth_raw.TOW_ID = zz_odn_temp_raw.TOW_ID
+    AND
+    zz_odn_depth_raw.TIMESTAMP = zz_odn_temp_raw.TIMESTAMP
+);
---
+++ Database/Triggers/odn_raw_refresh.sql
@@ -0,0 +1,26 @@
+DELIMITER &&
+CREATE TRIGGER odn_raw_refresh
+AFTER INSERT 
+ON TOWS FOR EACH ROW
+BEGIN
+  TRUNCATE TABLE odn_data_raw;
+  INSERT INTO odn_data_raw
+    SELECT
+      zz_odn_depth_raw.VESSEL_ID,
+      zz_odn_depth_raw.FMCODE AS GEAR_TYPE,
+      zz_odn_depth_raw.TOW_ID,
+      zz_odn_depth_raw.TIMESTAMP,
+      zz_odn_depth_raw.LATITUDE,
+      zz_odn_depth_raw.LONGITUDE,
+      zz_odn_depth_raw.DEPTH,
+      zz_odn_temp_raw.TEMP
+    FROM 
+      zz_odn_depth_raw
+    INNER JOIN 
+      zz_odn_temp_raw
+    ON 
+      zz_odn_depth_raw.TOW_ID = zz_odn_temp_raw.TOW_ID
+      AND
+      zz_odn_depth_raw.TIMESTAMP = zz_odn_temp_raw.TIMESTAMP
+END&&
+DELIMITER ;
