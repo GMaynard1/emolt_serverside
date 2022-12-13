fa7f9e614a694b20f15cb8a4426059860417309c
George Maynard <george.maynard@noaa.gov>
2022-12-13 16:03:47 -0500

    added a way to access top directly from mysql console

--- MySQL_Performance_Monitoring.sql
+++ MySQL_Performance_Monitoring.sql
@@ -1,3 +1,6 @@
+## Adapted from
+## https://www.datadoghq.com/blog/monitoring-mysql-performance-metrics
+
 ## Query Throughput
 SHOW GLOBAL STATUS 
 WHERE 
@@ -19,3 +22,17 @@ WHERE
   schema_name IS NOT NULL
 GROUP BY 
   schema_name;
+  
+## Connection Status
+SHOW GLOBAL STATUS 
+WHERE 
+  VARIABLE_NAME LIKE 'Threads_connected'
+  OR VARIABLE_NAME LIKE 'Threads_running'
+  OR VARIABLE_NAME LIKE 'Connection_errors_internal'
+  OR VARIABLE_NAME LIKE 'Aborted_connects'
+  OR VARIABLE_NAME LIKE 'Connection_errors_max_connections';
+  
+## Resource usage
+SHOW processlist;
+
+\! top
